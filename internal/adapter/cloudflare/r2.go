88535ad79d7b3f9fa2f9b6b9983e9ce24633737c
Irwanto Siregar <irwanto.inovasi@gmail.com>
2025-12-15 11:28:47 +0700

    add: create service get list content - 1

---
+++ internal/adapter/cloudflare/r2.go
@@ -0,0 +1,66 @@
+package cloudflare
+
+import (
+	"bwanews/config"
+	"bwanews/internal/core/domain/entity"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gofiber/fiber/v2/log"
+)
+
+var code string
+var err error
+
+type CloudflareR2Adapter interface {
+	UploudImage(req *entity.FileUploadEntity) (string, error)
+}
+
+type cloudflareR2Adapter struct {
+	Client  *s3.Client
+	Bucket  string
+	Baseurl string
+}
+
+func NewCloudflareR2Adapter(client *s3.Client, cfg *config.Config) CloudflareR2Adapter {
+	clientBase := s3.NewFromConfig(cfg.LoadAwsConfig(), func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", cfg.R2.AccountID))
+	})
+	
+	return &cloudflareR2Adapter{
+		Client:  clientBase,
+		Bucket:  cfg.R2.Name,
+		Baseurl: cfg.R2.PublicUrl,
+	}
+}
+
+// UploudImage implements the CloudflareR2Adapter interface; currently returns a not-implemented error.
+func (c *cloudflareR2Adapter) UploudImage(req *entity.FileUploadEntity) (string, error) {
+	openedFile, err := os.Open(req.Path)
+
+	if err != nil {
+		code = "[CLOUD FLARE R2] UploadImage - 1"
+		log.Errorw(code, err)
+		return "", err
+	}
+
+	defer openedFile.Close()
+
+	_, err = c.Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(c.Bucket),
+		Key: aws.String(req.Name),
+		Body: openedFile,
+		ContentType: aws.String("image/jpeg"),
+	})
+
+	if err != nil {
+		code = "[CLOUD FLARE R2] UploadImage - 2"
+		log.Errorw(code, err)
+		return "", err
+	}
+
+	return fmt.Sprintf("%s/%s", c.Baseurl, req.Name), nil
+}
